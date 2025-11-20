83e0531f809997d470ecfe41e679366559a514da
Dylan Almaras <dylanalmaras72@gmail.com>
2025-11-20 10:58:38 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,9 +11,11 @@ let n2 = 0
 let n1 = 0
 let numero = 0
 numero = 1
-while (numero == 0) {
-    for (let index = 0; index < 9; index++) {
-        basic.showNumber(numero)
-        numero += 1
-    }
+while (n1 == 0) {
+    basic.showNumber(numero)
+    numero += 1
+}
+while (n2 == 0) {
+    basic.showNumber(numero)
+    numero += 1
 }
