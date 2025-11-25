67b5ba7f4fef2aa0ae3ec8f011a58559d955f8fd
Dylan Almaras <dylanalmaras72@gmail.com>
2025-11-25 10:55:34 -0300

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    n1 += numero
-})
-input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-    n2 += numero
-})
-let n2 = 0
 let n1 = 0
-let numero = 0
-numero = 1
-while (n1 == 0) {
+let n2 = 0
+let numero = 1
+while (numero <= 11) {
     basic.showNumber(numero)
     numero += 1
     if (numero == 10) {
