a465e27f3d1d64dc7fd0d59482dcba19cb26d137
Dylan Almaras <dylanalmaras72@gmail.com>
2025-11-26 20:51:39 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
-let n1 = 0
+input.onButtonPressed(Button.A, function () {
+    n1 += numero
+})
+input.onButtonPressed(Button.AB, function () {
+    total = n1 + n2
+    basic.showNumber(total)
+})
+input.onButtonPressed(Button.B, function () {
+    n2 += numero
+})
 let n2 = 0
-let numero = 1
-while (numero <= 11) {
+let total = 0
+let n1 = 0
+let numero = 0
+numero = 1
+while (numero <= 10) {
     basic.showNumber(numero)
     numero += 1
-    if (numero == 10) {
+    if (numero > 9) {
         numero = 1
-        basic.showNumber(numero)
-        numero += 1
     }
 }
+
