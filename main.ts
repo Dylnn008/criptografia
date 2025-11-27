2351099ab5ebd8066c4a2b3345358d82a397e33b
Dylan Almaras <dylanalmaras72@gmail.com>
2025-11-27 10:26:25 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,24 +2,34 @@ input.onButtonPressed(Button.A, function () {
     n1 += numero
 })
 input.onButtonPressed(Button.AB, function () {
-    total = n1 + n2
-    basic.showNumber(total)
-    n1 = 0
-    n2 = 0
-    numero = 1
+    detener = false
 })
 input.onButtonPressed(Button.B, function () {
     n2 += numero
 })
 let n2 = 0
-let total = 0
 let n1 = 0
+let total = 0
 let numero = 0
+let detener = false
+detener = true
 numero = 1
 while (numero <= 10) {
     basic.showNumber(numero)
     numero += 1
     if (numero > 9) {
         numero = 1
+    }
+    if (detener != true) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        total = n1 + n2
+        basic.showNumber(total)
+        break;
     }
 }
